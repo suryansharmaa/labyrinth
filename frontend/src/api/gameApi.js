bb7de4df609f54d2a52241f55f520b181d59ac0f
Suryansh Sharma <suryansharmaa@gmail.com>
2026-09-21 00:03:38 +0530

    debugging deployment

--- frontend/src/api/gameApi.js
+++ frontend/src/api/gameApi.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:8080";
+console.log("NETLIFY ENV VAR:", import.meta.env.VITE_API_URL);
 
 const API = axios.create({
   baseURL: `${API_BASE}/api/rooms`,
